d562a6dbbd9dd9fcb2c335543a5e20e1eb1fc1b0
Omar De La Hoz <omar.dlhz@hotmail.com>
2017-05-06 21:39:09 -0400

    Initial Commit

---
+++ watchat WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  watchat WatchKit Extension
+//
+//  Created by Omar Dlhz on 5/6/17.
+//  Copyright © 2017 Omar De La Hoz. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ watchat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watchat
+//
+//  Created by Omar Dlhz on 5/6/17.
+//  Copyright © 2017 Omar De La Hoz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
